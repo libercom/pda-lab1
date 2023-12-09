9eb7367aeb0cba8e2cebb0603d7f911edc7c258c
Vasile Ignat <ignatvasea@gmail.com>
2023-12-09 15:37:52 +0200

    added middleware to count number of requests

---
+++ MangaStore.Catalog/src/Middleware/RequestCountMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Prometheus;
+
+namespace MangaStore.Catalog.Middleware
+{
+    public class RequestCountMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        private static readonly Counter RequestCounter = Metrics.CreateCounter("catalog_requests_total", "Count of requests to the Catalog API");
+
+        public RequestCountMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            // Increment the counter for each incoming request
+            RequestCounter.Inc();
+
+            // Call the next middleware in the pipeline
+            await _next(context);
+        }
+    }
+}
---
+++ MangaStore.Inventory/src/Middleware/RequestCountMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using Prometheus;
+using System.Diagnostics.Metrics;
+
+namespace MangaStore.Catalog.Middleware
+{
+    public class RequestCountMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        private static readonly Counter RequestCounter = Metrics.CreateCounter("inventory_requests_total", "Count of requests to the Inventory API");
+
+        public RequestCountMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            RequestCounter.Inc();
+
+            await _next(context);
+        }
+    }
+}
